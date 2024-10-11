c44ed2d7edfe38938433e0aac6235cd9fa08af33
Samyakt <isamyakt17@gmail.com>
2024-10-11 15:16:27 +0530

    in memory database commit

---
+++ src/db/index.ts
@@ -0,0 +1,99 @@
+interface UserBalance {
+  balance: number;
+  locked: number;
+}
+
+export const INR_BALANCES: { [key: string]: UserBalance } = {
+  user1: {
+    balance: 10,
+    locked: 0,
+  },
+  user2: {
+    balance: 20,
+    locked: 10,
+  },
+};
+
+
+interface Orders {
+  [userId: string]: number;
+}
+
+interface PriceLevel {
+  total: number;
+  orders: Orders;
+}
+
+interface Outcome {
+  [price: string]: PriceLevel;
+}
+
+interface Contract {
+  yes: Outcome;
+  no: Outcome;
+}
+
+interface OrderBook {
+  [contractId: string]: Contract;
+}
+
+export const ORDERBOOK: OrderBook = {
+  BTC_USDT_10_Oct_2024_9_30: {
+    yes: {
+      "9.5": {
+        total: 12,
+        orders: {
+          user1: 2,
+          user2: 10,
+        },
+      },
+      "8.5": {
+        total: 12,
+        orders: {
+          user1: 3,
+          user2: 3,
+          user3: 6,
+        },
+      },
+    },
+    no: {},
+  },
+};
+
+
+interface StockPosition {
+  quantity: number;
+  locked: number;
+}
+
+interface StockOutcome {
+  yes?: StockPosition;
+  no?: StockPosition;
+}
+
+interface UserStockBalances {
+  [contractId: string]: StockOutcome;
+}
+
+interface StockBalances {
+  [userId: string]: UserStockBalances;
+}
+
+const STOCK_BALANCES: StockBalances = {
+  user1: {
+    BTC_USDT_10_Oct_2024_9_30: {
+      yes: {
+        quantity: 1,
+        locked: 0,
+      },
+    },
+  },
+  user2: {
+    BTC_USDT_10_Oct_2024_9_30: {
+      no: {
+        quantity: 3,
+        locked: 4,
+      },
+    },
+  },
+};
