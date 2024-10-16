9c9a8b361fee42222210b304451962b585f3814d
Samyakt <isamyakt17@gmail.com>
2024-10-16 07:58:33 +0530

    app listening on 3000 port

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import express, { json } from "express";
-import cors from "cors";
-import balanceRouter from "./balance";
-
-const app = express();
-
-app.use(cors());
-app.use(json());
-
-app.use("/balance", balanceRouter);
+import app from "./app";
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
